e35bec236be9645affcb740265daf442d1d59707
Alexander Adedeji <alexadedeji15@gmail.com>
2022-01-02 16:20:24 +0100

    smart contact deployed

--- smart-contract/contracts/Transaction.sol
+++ smart-contract/contracts/Transaction.sol
@@ -1,12 +1,15 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
-contract Tansactions {
+import "hardhat/console.sol";
+
+contract Transactions {
     uint256 transactionCount;
 
-
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-    struct TransferStruct{
+  
+    struct TransferStruct {
         address sender;
         address receiver;
         uint amount;
@@ -17,21 +20,18 @@ contract Tansactions {
 
     TransferStruct[] transactions;
 
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
-    function addToBlockchain(address payable reciever, uint amount, string memory message, string memory keyword) public {
-        transactionCount+=1;
-        transactions.push(TransferStruct(msg.sender, reciever, amount, message, block.timestamp, keyword));
-
-        emit Transfer(msg.sender, reciever, amount, message, block.timestamp, keyword);
-
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
-    function getAllTransaction ()public view returns(TransferStruct[] memory){
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
 
-    function getTransactionCount() public view returns (uint256){
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
 }
-
--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -2,20 +2,22 @@ require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/pdC0IZJTTPj3rKGDS1rF1rjg7oPUMHz1",
+      accounts: [
+        "9777fbe0845531aaf883e1fbc02410606dbf2f44d35f4724f6f12d4d0bd40bc6",
+      ],
+    },
+  },
 };
